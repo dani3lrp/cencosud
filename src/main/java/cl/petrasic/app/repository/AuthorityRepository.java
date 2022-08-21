33bef4293ab2b03d91a4a43919c582e32ac51f5e
“Daniel” <danielroapetrasic@gmail.com>
2022-08-21 15:42:12 -0400

    Primer commit

---
+++ src/main/java/cl/petrasic/app/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package cl.petrasic.app.repository;
+
+import cl.petrasic.app.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/cl/petrasic/app/security/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Security configuration.
+ */
+package cl.petrasic.app.security;
